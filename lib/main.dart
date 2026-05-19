9981e4f5e3568ca7fd0e663a53b93b5fcf6297fb
Kanet.kam <>
2026-05-20 04:37:17 +0700

    feat: add app theme, snackbar undo, empty state, and dismiss background

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_expense_tracker_app/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 58, 211, 193),
+);
 void main() {
   runApp(
-    MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
   );
 }
